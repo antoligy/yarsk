6615ecac3471bc8d5d1e58d9bbddd6984706d282
Alex Wilson <antoligy@antoligy.com>
2016-04-30 21:51:57 +0100

    Silence warning about missing key.

--- app/components/Header/SubHeader.js
+++ app/components/Header/SubHeader.js
@@ -6,7 +6,8 @@ const SubHeader = ({children}) => {
   return <h2 className={styles.tagline}>
     {children.split(' ').map((word) => {
       const arr = word.split('');
-      return (<span><strong>{arr.shift()}</strong>{arr.join('')} </span>);
+      let firstLetter = arr.shift();
+      return (<span key={firstLetter}><strong>{firstLetter}</strong>{arr.join('')} </span>);
     })}
   </h2>;
 };
